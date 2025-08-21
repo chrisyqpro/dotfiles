59d11cb373f888f9e40dd70d3e49f1aa6e9f5ff9
Chris Qi <chrisyqpro@gmail.com>
2025-08-21 01:18:51 -0400

    feat(arkenfox): add arkenfox script

---
+++ arkenfox/user-overrides.js
@@ -0,0 +1,8 @@
+user_pref("browser.tabs.groups.enabled", true);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("extensions.pocket.enabled", false);
+
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.resistFingerprinting.letterboxing", true);
+user_pref("webgl.disabled", true);
+user_pref("privacy.spoof_english", 2);
