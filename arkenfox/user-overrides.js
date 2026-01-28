51940dc6e994457b72048eeb002234e8290a9fdd
Chris Qi <chrisyqpro@gmail.com>
2026-01-28 16:39:25 -0500

    feat(arkenfox): update user customization

--- arkenfox/user-overrides.js
+++ arkenfox/user-overrides.js
@@ -1,8 +1,19 @@
-user_pref("browser.tabs.groups.enabled", true);
+user_pref("browser.ml.enable", false);
+user_pref("browser.ml.checkForMemory", false);
+user_pref("browser.ml.linkPreview.shift", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.chat.page.footerBadge", false);
+user_pref("browser.ml.chat.page.menuBadge", false);
+user_pref("browser.ml.chat.shortcuts", false);
+user_pref("browser.ml.chat.shortcuts.custom", false);
+user_pref("browser.ml.chat.sidebar", false);
+
 user_pref("browser.preferences.moreFromMozilla", false);
-user_pref("extensions.pocket.enabled", false);
 
 user_pref("privacy.resistFingerprinting", true);
 user_pref("privacy.resistFingerprinting.letterboxing", true);
 user_pref("webgl.disabled", true);
 user_pref("privacy.spoof_english", 2);
+
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", true);
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", true);
